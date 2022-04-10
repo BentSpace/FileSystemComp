849d625d1693c6f5486e428027175e519cee6dc2
BentSpace <bentspacetime@gmail.com>
2022-04-10 11:10:19 -0700

    Traverse Directory Added

--- fileSystemComparison.py
+++ fileSystemComparison.py
@@ -2,34 +2,53 @@ import os
 import shutil
 
 homeDirectory = os.path.expanduser('~')
+directoryPathSingle = homeDirectory + "/singleRoot"
+directoryPathHy = homeDirectory + "/hierarchicalRoot"
 # def create_directory(directory_name, path)
 
 def singleRootFileSysCreate():
-    directoryPath = homeDirectory + "/singleRoot"
-    directoryExistsAlready = os.path.isdir(directoryPath)
+    directoryExistsAlready = os.path.isdir(directoryPathSingle)
     if directoryExistsAlready:
-        shutil.rmtree(directoryPath)
-    os.mkdir(directoryPath)
+        shutil.rmtree(directoryPathSingle)
+    os.mkdir(directoryPathSingle)
     i = 1
     while i <= 100:
-        f = open(directoryPath + "/file" + str(i) + ".txt", "w")
+        f = open(directoryPathSingle + "/file" + str(i) + ".txt", "w")
         f.close()
         i += 1
 
 def hyFileSysCreate():
-    directoryPath = homeDirectory + "/hierarchicalRoot"
-    directoryExistsAlready = os.path.isdir(directoryPath)
+    directoryExistsAlready = os.path.isdir(directoryPathHy)
     if directoryExistsAlready:
-        shutil.rmtree(directoryPath)
-    os.mkdir(directoryPath)
+        shutil.rmtree(directoryPathHy)
+    os.mkdir(directoryPathHy)
     i = 1
     while i <= 100:
         if i % 10 == 1:
-            newDir = directoryPath + "/files" + str(i) + "-" + str(i + 9) 
+            newDir = directoryPathHy + "/files" + str(i) + "-" + str(i + 9) 
             os.mkdir(newDir)
         f = open(newDir + "/file" + str(i) + ".txt", "w")
         f.close()
         i += 1
 
+def traverseDirectory(startDir):
+    fileDict = {}
+    dirDict = {}
+    for dirName, subdirList, fileList in os.walk(startDir):
+        for fileName in fileList:
+            fileSize = os.path.getsize(fileName)
+            fileDict.update({fileName: fileSize})
+        for subdir in subdirList:
+            dirSize = os.path.getsize(startDir + "/" + subdir)
+            dirDict.update({subdir: dirSize})
+    return fileDict, dirDict
+
+# def writeToFile():
+
+
 singleRootFileSysCreate()
 hyFileSysCreate()
+fileDict, dirDict = traverseDirectory(directoryPathSingle)
+print(fileDict, dirDict)
+fileDict, dirDict = traverseDirectory(directoryPathHy)
+print(fileDict, dirDict)
